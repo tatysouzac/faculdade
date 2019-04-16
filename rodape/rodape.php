a51d129f8b418d3db593e8ea6b355e9853f04111
Alexsandro <alexsandrofpf2011@gmail.com>
2019-04-16 12:35:04 -0300

    Subindo projeto oficial com pagína padrão para as demais telas

---
+++ rodape/rodape.php
@@ -0,0 +1,2 @@
+<i class="fas fa-copyright"></i>
+<span>2019 - T.A.D.S. 3º Período - Todos os Direitos reservados. </span>
---
+++ topo/topo.php
@@ -0,0 +1,22 @@
+<div class="topo">
+    <div class="row">
+        <div class="col-md-8">
+            <div class="logo">
+                <a class="link" href="./home.php">
+                    <h2>Faculdade Fatads</h2>
+                </a>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="cadastrar">
+                <button type="button" class="btn btn-success"> Cadastra-se </button>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="aluno">
+                <i class="fas fa-user-graduate"><span> SOU ALUNO </span></i>
+                <i class="fas fa-phone"><span> CONTATO </span></i>
+            </div>
+        </div>
+    </div>
+</div>
