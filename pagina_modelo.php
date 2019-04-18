a31436e77a232c268ff06fc4c96fafb667dfb626
Alexsandro <alexsandrofpf2011@gmail.com>
2019-04-17 23:42:14 -0300

    Ajuste do rodape

--- pagina_modelo.php
+++ pagina_modelo.php
@@ -87,14 +87,17 @@
                 </div>
             </div>
         </div>
+        <br>
         <!-- Colocar o seu formulario aqui -->
-        <div class="footer">
-            <div class="row">
-                <div class="col-md-12">
-                    <?php include 'rodape/rodape.php'; ?>
+        <footer class="rodape">
+            <div class="footer">
+                <div class="row">
+                    <div class="col-md-12">
+                        <?php include 'rodape/rodape.php'; ?>
+                    </div>
                 </div>
             </div>
-        </div>
+        </footer>
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
